b56221ad6eb0425976322f56af14f5aee041a8d9
Ajithkumar4 <89694620+Ajithkumar4@users.noreply.github.com>
2021-09-17 09:20:02 -0700

    Add files via upload

---
+++ Day17/BubbleSort.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+class BubbleSort
+{
+	public static void main(String []args)
+	{
+		int n,i,j,temp;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of integers to sorting :");
+		n=sc.nextInt();
+		int array[]=new int[n];
+		System.out.println("Enter " + n + " integers :");
+		for(i=0; i<n; i++)
+		array[i]=sc.nextInt();
+		for(i=0; i<(n-1); i++)
+		{
+			for(j=0; j<n-i-1; j++)
+			{
+				if(array[j]>array[j+1])
+				{
+					temp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
+				}
+			}
+		}
+		System.out.println("Sorted list of numbers :");
+		for(i=0; i<n; i++)
+		{
+			System.out.println(array[i]);
+		}
+	}
+}
